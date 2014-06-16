f1a385fa7fccbb50d998118b0b8fdf1f6d857d60
g2scott <simontupperscott@gmail.com>
2014-06-16 12:07:01 -0400

    remove firephp

--- main/application/config/autoload.php
+++ main/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'firephp', 'fireb');
+$autoload['libraries'] = array('database');
 
 
 /*
