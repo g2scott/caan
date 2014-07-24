86ec7785fca5ff1e532444366fff40bcb5ac172c
g2scott <simontupperscott@gmail.com>
2014-07-23 20:39:53 -0400

    facebook like

--- main/application/controllers/upload.php
+++ main/application/controllers/upload.php
@@ -131,7 +131,6 @@ class Upload extends CI_Controller {
 		$config['max_height']  = '0';
 
 		$this->load->library('upload', $config);
-		form_open_multipart();
 
 		if ( ! $this->upload->do_upload())
 		{
